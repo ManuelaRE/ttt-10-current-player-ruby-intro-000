021e937ee2ec5e0deaddf32d52c9d4b41a63a00e
Manuela Redinciuc <manutzu15@gmail.com>
2020-02-03 12:50:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   board.count{|move| move == "X" || move == "0"}
   turn = 0
 board.each {|move|
-  if move == "X" || move == "0" turn += 1
+  if move == "X" || move == "0"; turn += 1
   end
 }
 
